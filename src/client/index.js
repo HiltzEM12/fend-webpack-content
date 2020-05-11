292720a405fe756a6981104a890389c1e88eeeb1
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-11 13:44:41 -0400

    Used bable to translate ES6 files

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,9 @@
+//Import the needed functions from the javascript file.  
+//The referenced file must have an export statement
+//This is ES6 syntax and babel should be used to translate it
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
 alert("I exist!")
