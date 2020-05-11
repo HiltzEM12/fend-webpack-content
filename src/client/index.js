72a1fcb7d5217753dd9110cfd0fa8528d5b083f6
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-11 14:14:41 -0400

    Finished testing using another loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,9 @@
 //Import the needed functions from the javascript file.  
 //The referenced file must have an export statement
 //This is ES6 syntax and babel should be used to translate it
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import img from '../test.jpg';
 
 console.log(checkForName);
 
